41fe7f1e6147f7aa9241c3b6e05d8c087d4cff3d
Aprendiz <Aprendiz@BOGDFPCGMF4141.FormacionSena.RED>
2024-02-06 17:20:44 -0500

    primeras entidades

---
+++ app/entities.py
@@ -0,0 +1,10 @@
+class Carro():
+    placa = 'ADF456'
+    tipo_vehiculo = 'Camion'
+    
+    
+class Cliente():
+    nombre = 'jhosep'
+    documento = 1234567
+    licencia_conduccion = 'C3'
+    celular = 311111111   
